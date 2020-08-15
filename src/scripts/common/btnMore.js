f4b42634ac4ecbea734f65db7d4247338973fac8
“Евгений <sishnenko86@mail.ru>
2020-08-15 21:13:28 +0300

    вывел страницу

--- src/scripts/common/btnMore.js
+++ src/scripts/common/btnMore.js
@@ -1,9 +1,9 @@
 function btnMore() {
-  $(".news_all__item").slice(0, 4).show();
+  $(".more-js").slice(0, 4).show();
   $(".news_all__btn-more").on("click", function (e) {
     e.preventDefault();
-    $(".news_all__item:hidden").slice(0, 4).slideDown();
-    if ($(".news_all__item:hidden").length == 0) {
+    $(".more-js:hidden").slice(0, 4).slideDown();
+    if ($(".more-js:hidden").length == 0) {
       $(".news_all__btn-more").hide();
     }
   });
