584a35102482ed26825d863d94c6a18d141c8e36
Margo Dorokhova <ritka15@mail.ru>
2020-08-18 21:25:24 +0300

    ком

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,8 +1,13 @@
+console.log('log');
+
 // import "slick-carousel";
 // const slider = require("./common/slider");
 const btnMore = require("./common/btnMore");
 const burger = require("./common/burger");
 
 // slider();
-btnMore();
-burger();
+$(document).ready(function(){
+    btnMore();
+    burger();
+});
+
