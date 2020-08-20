3d1526e4ec6cc510a4180d1e0d3947f6738be79d
“Евгений <sishnenko86@mail.ru>
2020-08-20 23:13:38 +0300

    fix

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,4 +1,4 @@
-import "slick-carousel";
+// import "slick-carousel";
 // const slider = require("./common/slider");
 const btnMore = require("./common/btnMore");
 const burger = require("./common/burger");
