c44f9e8dc3356e5af7736e600d3f155b432e615c
“Евгений <sishnenko86@mail.ru>
2020-08-22 20:41:15 +0300

    fix

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,14 +2,12 @@
 // const slider = require("./common/slider");
 const btnMore = require("./common/btnMore");
 const burger = require("./common/burger");
-const hoverInfo = require("./common/hoverInfo");
 const clickForm = require("./common/clickForm");
 import AOS from "aos";
 
 $(document).ready(function () {
   btnMore();
   burger();
-  hoverInfo();
   clickForm();
   AOS.init();
 });
