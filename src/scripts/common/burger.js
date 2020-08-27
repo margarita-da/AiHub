3cc9d95e594c6fb2a041a3338157f4c72a5eb38a
“Евгений <sishnenko86@mail.ru>
2020-08-27 22:40:49 +0300

    fix menu

--- src/scripts/common/burger.js
+++ src/scripts/common/burger.js
@@ -1,9 +1,16 @@
 function burger() {
   $(".burger-menu").click(function () {
+    const distance = document
+      .querySelector(".burger-menu")
+      .getBoundingClientRect();
+    const left = distance.left;
+    const top = distance.top;
+    console.log(left, top);
     // $(this).toggleClass("menu-on");
     $(".menu").toggleClass("menu-active");
     $(".burger-menu").hide();
     $(".menu-close").toggleClass("menu-close-active");
+    $(".menu-close-active").css({ left: left + "px", top: 15 + top + "px" });
   });
   $(".menu-close, .block__menu__close").on("click", function () {
     $(".burger-menu").show();
